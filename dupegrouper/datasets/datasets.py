86d7ffcf4a506839423b94bb1350260bb5140829
Victor Autonell-Oiry <victor.autonell@hotmail.com>
2026-01-14 21:37:24 +0000

    feat: partial tests for ids

--- dupegrouper/datasets/datasets.py
+++ dupegrouper/datasets/datasets.py
@@ -14,19 +14,19 @@ COLUMNS = [
 ]
 
 DATA13 = [
-    [0, "123ab, OL5 9PL, UK", "bbab@example.com", "reddit", "spain", "married", 1, "rental", 2.4, 545, 5, 3],
-    [1, "99 Ambleside avenue park Road, ED3 3RT, Edinburgh, United Kingdom", "bb@example.com", "reddit", "spain", "married", 1, "rental", 2.4, 452, 6, 3,],
-    [2, "Calle Ancho, 12, 05688, Rioja, Navarra, Espana", "a@example.com", "facebook", "germany", "single", 2, "rental", 2.5, 623, 5, 3],
-    [3, "Calle Sueco, 56, 05688, Rioja, Navarra", "hellothere@example.com", "pinterest", "japan", "married", 0, "owner", 4.0, 2077, 305, 6],
-    [4, "4 Brinkworth Way, GH9 5KL, Edinburgh, United Kingdom", "b@example.com", "pinterest", "malaysia", "single", 0, "rental", 6.0, 3656, 1342, 7],
-    [5, "66b Porters street, OL5 9PL, Newark, United Kingdom", "bab@example.com", "flickr", "malaysia", "single", 0, "owner", 2.5, 4000, 25, 8],
-    [6, "C. Ancho 49, 05687, Navarra", "b@example.com", "reddit", "japan", "married", 1, "rental", 2.5, 1323, 132, 4],
-    [7, "Ambleside avenue Park Road ED3, UK", "hellthere@example.com", "reddit", "germany", "married", 0, "owner", 2.5, 509, 200, 2],
-    [8, "123ab, OL5 9PL, UK", "hellathere@example.com", "facebook", "japan", "single", 3, "owner", 2.5, 500, 300, 3],
-    [9, "123ab, OL5 9PL, UK", "irrelevant@hotmail.com", "google", "malaysia", "divorced", 1, "social housing", 2.5, 450, 15, 3],
-    [10, "37 Lincolnshire lane, GH9 5DF, Edinburgh, UK", "yet.another.email@msn.com", "flickr", "germany", "married", 1, "rental", 2.5, 345, 22, 3],
-    [11, "37 GH9, UK", "awesome_surfer_77@yahoo.com", "linkedin", "france", "married", 1, "rental", 2.7, 1045, 42, 4],
-    [12, "totally random non existant address", "fictitious@never.co.uk", "linkedin", "japan", "single", 0, "rental", 4.0, 1545, 62, 6],
+    [1, "123ab, OL5 9PL, UK", "bbab@example.com", "reddit", "spain", "married", 1, "rental", 2.4, 545, 5, 3],
+    [2, "99 Ambleside avenue park Road, ED3 3RT, Edinburgh, United Kingdom", "bb@example.com", "reddit", "spain", "married", 1, "rental", 2.4, 452, 6, 3,],
+    [3, "Calle Ancho, 12, 05688, Rioja, Navarra, Espana", "a@example.com", "facebook", "germany", "single", 2, "rental", 2.5, 623, 5, 3],
+    [4, "Calle Sueco, 56, 05688, Rioja, Navarra", "hellothere@example.com", "pinterest", "japan", "married", 0, "owner", 4.0, 2077, 305, 6],
+    [5, "4 Brinkworth Way, GH9 5KL, Edinburgh, United Kingdom", "b@example.com", "pinterest", "malaysia", "single", 0, "rental", 6.0, 3656, 1342, 7],
+    [6, "66b Porters street, OL5 9PL, Newark, United Kingdom", "bab@example.com", "flickr", "malaysia", "single", 0, "owner", 2.5, 4000, 25, 8],
+    [7, "C. Ancho 49, 05687, Navarra", "b@example.com", "reddit", "japan", "married", 1, "rental", 2.5, 1323, 132, 4],
+    [8, "Ambleside avenue Park Road ED3, UK", "hellthere@example.com", "reddit", "germany", "married", 0, "owner", 2.5, 509, 200, 2],
+    [9, "123ab, OL5 9PL, UK", "hellathere@example.com", "facebook", "japan", "single", 3, "owner", 2.5, 500, 300, 3],
+    [10, "123ab, OL5 9PL, UK", "irrelevant@hotmail.com", "google", "malaysia", "divorced", 1, "social housing", 2.5, 450, 15, 3],
+    [11, "37 Lincolnshire lane, GH9 5DF, Edinburgh, UK", "yet.another.email@msn.com", "flickr", "germany", "married", 1, "rental", 2.5, 345, 22, 3],
+    [12, "37 GH9, UK", "awesome_surfer_77@yahoo.com", "linkedin", "france", "married", 1, "rental", 2.7, 1045, 42, 4],
+    [13, "totally random non existant address", "fictitious@never.co.uk", "linkedin", "japan", "single", 0, "rental", 4.0, 1545, 62, 6],
 ]
 
 # fmt: on
